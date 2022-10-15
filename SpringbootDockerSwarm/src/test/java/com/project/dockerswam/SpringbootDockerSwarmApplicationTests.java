185527608950b50ce5bb4a2957a8da755c818c44
Shubham Srivastava <shubhamsrivastav9991@gmail.com>
2022-10-15 20:08:19 +0530

    Practice Project4 Solution

---
+++ SpringbootDockerSwarm/src/test/java/com/project/dockerswam/SpringbootDockerSwarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dockerswam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerSwarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
